b379ecf70057f51e68651f79a14feba5999f9a3f
Muhammet <muhammetvural@anadolu.edu.tr>
2023-05-22 14:27:30 +0300

    dark theme e geçiş bir sefer yapılıyor

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:quizz_app_with_dark_theme/screens/main_menu.dart';
+import 'package:quizz_app_with_dark_theme/screens/quizz_screen.dart';
 import 'package:quizz_app_with_dark_theme/theme/theme_constants.dart';
 import 'package:quizz_app_with_dark_theme/theme/theme_manager.dart';
 
@@ -17,6 +18,26 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
+    @override
+  void dispose() {
+    _themeManager.removeListener(themeListener);
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    _themeManager.addListener(themeListener);
+    super.initState();
+  }
+
+  themeListener(){
+    if(mounted){
+      setState(() {
+
+      });
+    }
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -26,6 +47,67 @@ class _MyAppState extends State<MyApp> {
       darkTheme: darkTheme,
       themeMode: _themeManager.themeMode,
       home: const MainMenu()
+    );
+  }
+}
+
+
+
+class MainMenu extends StatefulWidget {
+  const MainMenu({Key? key}) : super(key: key);
+
+  @override
+  _MainMenuState createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu> {
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Quiz App'), actions: [
+        Switch(value: _themeManager.themeMode == ThemeMode.dark, onChanged: (newValue){
+          _themeManager.toggleTheme(newValue);
+
+        })
+      ],),
+      
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 48.0,
+          horizontal: 12.0,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    //Navigating the the Quizz Screen
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => QuizzScreen(),
+                        ));
+                  },
+                  child: const Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+                    child: Text(
+                      "Start the Quizz",
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/screens/main_menu.dart
+++ lib/screens/main_menu.dart
@@ -1,85 +1,65 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:quizz_app_with_dark_theme/screens/quizz_screen.dart';
-import 'package:quizz_app_with_dark_theme/theme/theme_manager.dart';
 
-import '../ui/shared/color.dart';
+// import 'package:flutter/material.dart';
+// import 'package:quizz_app_with_dark_theme/screens/quizz_screen.dart';
+// import 'package:quizz_app_with_dark_theme/theme/theme_manager.dart';
 
 
-class MainMenu extends StatefulWidget {
-  const MainMenu({Key? key}) : super(key: key);
+// class MainMenu extends StatefulWidget {
+//   const MainMenu({Key? key}) : super(key: key);
 
-  @override
-  _MainMenuState createState() => _MainMenuState();
-}
+//   @override
+//   _MainMenuState createState() => _MainMenuState();
+// }
+//   ThemeManager _themeManager = ThemeManager();
 
-class _MainMenuState extends State<MainMenu> {
+// class _MainMenuState extends State<MainMenu> {
 
-    @override
-  void dispose() {
-    _themeManager.removeListener(themeListener);
-    super.dispose();
-  }
+  
 
-  @override
-  void initState() {
-    _themeManager.addListener(themeListener);
-    super.initState();
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text('Quiz App'), actions: [
+//         Switch(value: _themeManager.themeMode != ThemeMode.dark, onChanged: (newValue){
+//           _themeManager.toggleTheme(newValue);
 
-  themeListener(){
-    if(mounted){
-      setState(() {
-
-      });
-    }
-  }
-
-  ThemeManager _themeManager = ThemeManager();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Quiz App'), actions: [
-        Switch(value: _themeManager.themeMode == ThemeMode.dark, onChanged: (newValue){
-          _themeManager.toggleTheme(newValue);
-
-        })
-      ],),
-      backgroundColor: AppColor.pripmaryColor,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 48.0,
-          horizontal: 12.0,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    //Navigating the the Quizz Screen
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => QuizzScreen(),
-                        ));
-                  },
-                  child: const Padding(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
-                    child: Text(
-                      "Start the Quizz",
-                    ),
-                  ),
-                ),
-              ),
-            ),
+//         })
+//       ],),
+      
+//       body: Padding(
+//         padding: const EdgeInsets.symmetric(
+//           vertical: 48.0,
+//           horizontal: 12.0,
+//         ),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             Expanded(
+//               child: Center(
+//                 child: ElevatedButton(
+//                   onPressed: () {
+//                     //Navigating the the Quizz Screen
+//                     Navigator.push(
+//                         context,
+//                         MaterialPageRoute(
+//                           builder: (context) => QuizzScreen(),
+//                         ));
+//                   },
+//                   child: const Padding(
+//                     padding:
+//                         EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+//                     child: Text(
+//                       "Start the Quizz",
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
             
-          ],
-        ),
-      ),
-    );
-  }
-}
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
