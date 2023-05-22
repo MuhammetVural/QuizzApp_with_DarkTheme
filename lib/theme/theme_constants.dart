1d9e51a0c567e4956e7d75c081c56bdb35b5cb2b
Muhammet <muhammetvural@anadolu.edu.tr>
2023-05-22 17:23:36 +0300

    Theme özellikleri atandi

--- lib/theme/theme_constants.dart
+++ lib/theme/theme_constants.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
-const COLOR_PRIMARY = Colors.deepOrangeAccent;
+const COLOR_PRIMARY = Colors.orange;
 const COLOR_ACCENT = Colors.orange;
 
 
 
 ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
-  primaryColor: COLOR_PRIMARY,
+  primarySwatch: COLOR_PRIMARY,
+  textTheme: const TextTheme(
+    
+  ),
+  
   floatingActionButtonTheme: const FloatingActionButtonThemeData(
-    backgroundColor: COLOR_ACCENT
+    backgroundColor: COLOR_PRIMARY,
+  foregroundColor: Colors.white
   ),
   elevatedButtonTheme: ElevatedButtonThemeData(
     style: ButtonStyle(
-      padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 40, vertical: 20),),
+      padding: MaterialStateProperty.all(const EdgeInsets.symmetric(horizontal: 60,),),
       shape: MaterialStateProperty.all(RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10)
       )),
-      backgroundColor: MaterialStateProperty.all(COLOR_ACCENT)
+      backgroundColor: MaterialStateProperty.all(COLOR_PRIMARY)
     )
   )
   
